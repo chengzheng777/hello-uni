b0241f6ddc0ff81a3c68df12b7a22f5f0100e652
程政 <60911364+chengzheng777@users.noreply.github.com>
2021-11-08 13:47:53 +0800

    2

---
+++ src/env/env.dev.js
@@ -0,0 +1,16 @@
+/**
+ * @Description: 环境变量配置 dev
+ * @Author: LXG
+ * @Date: 2021-08-11
+ * @Editors: LXG
+ * @LastEditTime: 2021-08-11
+ */
+
+export default {
+	ENV: 'development',
+	ENVIRONMENT: 'dev',
+
+	API_BASE_URL: 'https://qqzq.baoan.gov.cn/balocal-api-dev',
+
+
+}

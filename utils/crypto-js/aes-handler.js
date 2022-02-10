6961cf9b5ffa6cd8668b31b96397b6e13f9a097a
林晓刚1995 <40429540+linxg1995@users.noreply.github.com>
2022-02-10 19:54:41 +0800

    基于crypto-js，封装通用的aes加解密函数；

--- utils/crypto-js/aes-handler.js
+++ utils/crypto-js/aes-handler.js
@@ -3,7 +3,7 @@
  * @Author: LXG
  * @Date: 2022-02-09
  * @Editors: LXG
- * @LastEditTime: 2022-02-09
+ * @LastEditTime: 2022-02-10
  */
 
 const {
@@ -19,49 +19,113 @@ console.log(AES, enc, mode, pad)
  * @param {Object} ciphetext 密文
  * @param {Object} key 密钥
  * @param {object} option 配置选项
- * @returns {string} 明文
- */
-function decrypt(ciphetext, key, option = {}) { }
-
-/**
- * @description 加密
- * @param {string} plaintext 明文
- * @param {string} key 密钥
- * @param {object} option 配置选项
- * @param {string} [option.mode='ECB'] option.mode 加密模式 -
+ * @param {string} [option.mode='CBC'] option.mode 加密模式 -
  *     'CBC'
  *     'CFB'
  *     'CTR'
  *     'ECB'
  *     'OFB'
- * @param {string} [option.iv=''] option.iv 明文偏移量
- * @param {string} [option.padding='Pkcs7'] option.padding 字节填充方式 -
+ * @param {string} [option.iv=key.substring(0, 16)] option.iv 密钥偏移量(字节长为16)
+ * @param {string} [option.padding='ZeroPadding'] option.padding 字节填充方式 -
  *     'NoPadding': 不填充
  *     'Pkcs7': 缺几个字节就填几个缺的字节数
  *     'ZeroPadding': 全部填充0x00
- * @returns {string} 密文
+ * @param {string} [option.format='Base64'] option.format 密文编码格式 -
+ *     'Base64'
+ *     'Hex': 十六进制
+ * @returns {string} 明文
  */
-function encrypt(plaintext, key, option = {}) {
-	if (!plaintext) {
-		console.warn(`encrypt: 'plaintext' isn't defined.`)
+function decrypt(ciphetext, key, option = {}) {
+	if (!ciphetext) {
+		console.warn(`encrypt: 'ciphetext' isn't defined.`)
 		return;
 	}
+
 	if (!key) {
 		console.warn(`encrypt: 'key' isn't defined.`)
 		return;
 	}
 
-	const PLAINTEXT_BYTES = enc.Utf8.parse(plaintext)
-	const KEY_BYTES = enc.Utf8.parse(key)
-	const AES_IV = enc.Utf8.parse(option.iv || '')
-	const AES_MODE = mode[option.mode || 'ECB'] || mode.ECB
-	const AES_PADDING = pad[option.padding || 'Pkcs7'] || pad.Pkcs7
-	let ciphe = AES.encrypt(PLAINTEXT_BYTES, KEY_BYTES, {
-		iv: AES_IV,
-		mode: AES_MODE,
-		padding: AES_PADDING,
-	})
-	return ciphe.toString();
+	const cipher_bytes = (enc[option.format || 'Base64'] || enc.Base64).parse(ciphetext)
+	const cipher_base64 = enc.Base64.stringify(cipher_bytes)
+	const key_bytes = enc.Utf8.parse(key)
+
+	const aesConfig = {
+		mode: mode[option.mode || 'CBC'] || mode.CBC,
+		iv: '',
+		padding: pad[option.padding || 'ZeroPadding'] || pad.ZeroPadding,
+	}
+	if (aesConfig.mode != mode.ECB) {
+		aesConfig.iv = enc.Utf8.parse(option.iv || key.substring(0, 16))
+		if (aesConfig.iv.sigBytes !== 16) {
+			console.warn(`encrypt: invalid iv_bytes.`)
+			return;
+		}
+	}
+
+	let res = AES.decrypt(cipher_base64, key_bytes, aesConfig)
+	try {
+		return res.toString(enc.Utf8);
+	} catch (error) {
+		console.error(`decrypt: ${error}`)
+		return;
+	}
+}
+
+/**
+ * @description 加密
+ * @param {string} message 明文
+ * @param {string} key 密钥(字节长为 16/24/32)
+ * @param {object} option 配置选项
+ * @param {string} [option.mode='CBC'] option.mode 加密模式 -
+ *     'CBC'
+ *     'CFB'
+ *     'CTR'
+ *     'ECB'
+ *     'OFB'
+ * @param {string} [option.iv=key.substring(0, 16)] option.iv 密钥偏移量(字节长为16)
+ * @param {string} [option.padding='ZeroPadding'] option.padding 字节填充方式 -
+ *     'NoPadding': 不填充
+ *     'Pkcs7': 缺几个字节就填几个缺的字节数
+ *     'ZeroPadding': 全部填充0x00
+ * @param {string} [option.format='Base64'] option.format 密文编码格式 -
+ *     'Base64'
+ *     'Hex': 十六进制
+ * @returns {string} 密文
+ */
+function encrypt(message, key, option = {}) {
+	if (!message) {
+		console.warn(`encrypt: 'message' isn't defined.`)
+		return;
+	}
+
+	if (!key) {
+		console.warn(`encrypt: 'key' isn't defined.`)
+		return;
+	}
+
+	const message_bytes = enc.Utf8.parse(message)
+	const key_bytes = enc.Utf8.parse(key)
+	if ([16, 24, 32].every(l => l !== key_bytes.sigBytes)) {
+		console.warn(`encrypt: invalid key_bytes.`)
+		return;
+	}
+
+	const aesConfig = {
+		mode: mode[option.mode || 'CBC'] || mode.CBC,
+		iv: '',
+		padding: pad[option.padding || 'ZeroPadding'] || pad.ZeroPadding,
+	}
+	if (aesConfig.mode != mode.ECB) {
+		aesConfig.iv = enc.Utf8.parse(option.iv || key.substring(0, 16))
+		if (aesConfig.iv.sigBytes !== 16) {
+			console.warn(`encrypt: invalid iv_bytes.`)
+			return;
+		}
+	}
+
+	let cipherParams = AES.encrypt(message_bytes, key_bytes, aesConfig)
+	return cipherParams.ciphertext.toString(enc[option.format || 'Base64'] || enc.Base64);
 }
 
 export {
